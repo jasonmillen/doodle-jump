3fdffa5cb864f11cb291d2bb0c55e83347c0e400
Jason Millen <JasonMillen@Jasons-MacBook-Air.local>
2014-09-13 21:07:58 -0400

    Initial Commit

---
+++ Doodle Jump/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Doodle Jump
+//
+//  Created by Jason Millen on 9/13/14.
+//  Copyright (c) 2014 Jason Millen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
